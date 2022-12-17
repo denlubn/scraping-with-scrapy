b667d8c98b0b96859789af678334be91b293a51e
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-12-17 18:50:45 +0200

    Scrapy pagination

--- ecommerce/spiders/products.py
+++ ecommerce/spiders/products.py
@@ -1,5 +1,9 @@
 import scrapy
+from scrapy import Selector
 from scrapy.http import Response
+from selenium import webdriver
+from selenium.common import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
 
 
 class ProductsSpider(scrapy.Spider):
@@ -8,6 +12,13 @@ class ProductsSpider(scrapy.Spider):
     start_urls = [
         "https://webscraper.io/test-sites/e-commerce/static/computers/laptops"
     ]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
+    def close(self, reason):
+        self.driver.close()
 
     def parse(self, response: Response, **kwargs):
         for product in response.css(".thumbnail"):
@@ -19,4 +30,37 @@ class ProductsSpider(scrapy.Spider):
                 "num_of_reviews": int(
                     product.css(".ratings > p.pull-right::text").get().split()[0]
                 ),
+                "additional_info": {
+                    "hdd_prices": self._parse_hdd_block_prices(response, product)
+                }
             }
+
+        next_page = response.css(".pagination > li")[-1].css("a::attr(href)").get()
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def _parse_hdd_block_prices(self, response: Response, product: Selector) -> dict[str, float]:
+        detailed_url = response.urljoin(product.css(".title::attr(href)").get())
+        self.driver.get(detailed_url)
+        swatches = self.driver.find_element(By.CLASS_NAME, "swatches")
+        buttons = swatches.find_elements(By.TAG_NAME, "button")
+
+        prices = {}
+
+        for button in buttons:
+            if not button.get_property("disabled"):
+                is_clickable = False
+                while not is_clickable:
+                    try:
+                        button.click()
+                    except ElementClickInterceptedException:
+                        continue
+                    else:
+                        is_clickable = True
+
+                prices[button.get_property("value")] = float(self.driver.find_element(
+                    By.CLASS_NAME, "price"
+                ).text.replace("$", ""))
+
+        return prices
